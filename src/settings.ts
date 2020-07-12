05fa2ddc547db45c1e713525adbf78aa1acda5cc
Rodrigo Zamperlini <rzamperlini@gmail.com>
2020-07-12 16:41:35 -0300

    Drawing a triangle to understand the concepts behind building an polygon.

--- src/settings.ts
+++ src/settings.ts
@@ -29,20 +29,11 @@
 import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
 import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
+export class ArrowSettings {
+  public arrowColor: string = "blue";
+  public arrowThickness: number = 2;
+}
 export class VisualSettings extends DataViewObjectsParser {
-      public dataPoint: dataPointSettings = new dataPointSettings();
-      }
-
-    export class dataPointSettings {
-     // Default color
-      public defaultColor: string = "";
-     // Show all
-      public showAllDataPoints: boolean = true;
-     // Fill
-      public fill: string = "";
-     // Color saturation
-      public fillRule: string = "";
-     // Text Size
-      public fontSize: number = 12;
-     }
+  public arrow: ArrowSettings = new ArrowSettings();
+}
 
--- src/visual.ts
+++ src/visual.ts
@@ -31,43 +31,82 @@ import powerbi from "powerbi-visuals-api";
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
 import VisualObjectInstance = powerbi.VisualObjectInstance;
 import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 
 import { VisualSettings } from "./settings";
+import * as d3 from "d3";
+
+type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
+
 export class Visual implements IVisual {
-    private target: HTMLElement;
-    private updateCount: number;
-    private settings: VisualSettings;
-    private textNode: Text;
+    
+    private host: IVisualHost;
+    private svg: Selection<SVGElement>;
+    private container: Selection<SVGElement>;
+    private arrow: Selection<SVGElement>;
+    private textValue: Selection<SVGElement>;
+    private textLabel: Selection<SVGElement>;
+    private visualSettings: VisualSettings;
 
     constructor(options: VisualConstructorOptions) {
         console.log('Visual constructor', options);
-        this.target = options.element;
-        this.updateCount = 0;
-        if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
+        
+        this.svg = d3.select(options.element)
+            .append('svg')
+            .classed('ArrowCard', true);
+        this.container = this.svg.append("g")
+            .classed('container', true);
+        this.arrow = this.container.append("polygon")
+            .classed('arrow', true);
+        this.textValue = this.container.append("text")
+            .classed("textValue", true);
+        this.textLabel = this.container.append("text")
+            .classed("textLabel", true);
     }
 
     public update(options: VisualUpdateOptions) {
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
-        }
-    }
+        let dataView: DataView = options.dataViews[0];
+        let width: number = options.viewport.width;
+        let height: number = options.viewport.height;
 
-    private static parseSettings(dataView: DataView): VisualSettings {
-        return <VisualSettings>VisualSettings.parse(dataView);
+        this.svg.attr("width", width);
+        this.svg.attr("height", height);
+
+        this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
+
+        this.visualSettings.arrow.arrowThickness = Math.max(0, this.visualSettings.arrow.arrowThickness);
+        this.visualSettings.arrow.arrowThickness = Math.min(10, this.visualSettings.arrow.arrowThickness);
+
+        this.arrow
+            .attr("points", width/2+",0 "+
+                            "0,"+height/2 + " "+
+                            width+","+height/2+ " "+
+                            width/2+",0")
+            .style("fill", this.visualSettings.arrow.arrowColor)
+            .style("stroke", "black")
+            .style("strokeWidth", this.visualSettings.arrow.arrowThickness);
+
+
+        let fontSizeValue: number = Math.min(width, height) / 5;
+        this.textValue
+            .text(<string>dataView.single.value)
+            .attr("x", "50%")
+            .attr("y", "50%")
+            .attr("dy", "0.35em")
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSizeValue + "px");
+        let fontSizeLabel: number = fontSizeValue / 4;
+        this.textLabel
+            .text(dataView.metadata.columns[0].displayName)
+            .attr("x", "50%")
+            .attr("y", height / 2)
+            .attr("dy", fontSizeValue / 1.2)
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSizeLabel + "px");
     }
 
     /**
@@ -76,6 +115,7 @@ export class Visual implements IVisual {
      *
      */
     public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+        const settings: VisualSettings = this.visualSettings || <VisualSettings>VisualSettings.getDefault();
+        return VisualSettings.enumerateObjectInstances(settings, options);
     }
 }
